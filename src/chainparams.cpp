5d598e46588ebac07f8e081e6ed3a39f8a7d35f6
Patrick Lodder <patricklodder@users.noreply.github.com>
2022-09-29 22:07:47 +0200

    hack: revert changes to regtest address bytes
    NOT FOR MERGING
    
    Minimal change to be able to demonstrate regression testing.
    This needs to be done properly.

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -479,9 +479,9 @@ public:
             0
         };
 
-        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,113);
+        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,111);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,196);
-        base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1,241);
+        base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1,239);
         base58Prefixes[EXT_PUBLIC_KEY] = {0x04, 0x35, 0x87, 0xCF};
         base58Prefixes[EXT_SECRET_KEY] = {0x04, 0x35, 0x83, 0x94};
 
