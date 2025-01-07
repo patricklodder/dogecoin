ceddab5ef64f47462389b5ab52386eccf618dd18
Patrick Lodder <patricklodder@users.noreply.github.com>
2025-01-07 17:04:45 -0500

    depends: update miniupnpc to 2.2.8

--- depends/packages/miniupnpc.mk
+++ depends/packages/miniupnpc.mk
@@ -1,8 +1,8 @@
 package=miniupnpc
-$(package)_version=2.0.20170509
+$(package)_version=2.2.8
 $(package)_download_path=http://miniupnp.free.fr/files
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
-$(package)_sha256_hash=d3c368627f5cdfb66d3ebd64ca39ba54d6ff14a61966dbecb8dd296b7039f16a
+$(package)_sha256_hash=05b929679091b9921b6b6c1f25e39e4c8d1f4d46c8feb55a412aa697aee03a93
 
 define $(package)_set_vars
 $(package)_build_opts=CC="$($(package)_cc)"
@@ -18,11 +18,11 @@ define $(package)_preprocess_cmds
 endef
 
 define $(package)_build_cmds
-	$(MAKE) libminiupnpc.a $($(package)_build_opts)
+	$(MAKE) build/libminiupnpc.a $($(package)_build_opts)
 endef
 
 define $(package)_stage_cmds
 	mkdir -p $($(package)_staging_prefix_dir)/include/miniupnpc $($(package)_staging_prefix_dir)/lib &&\
 	install *.h $($(package)_staging_prefix_dir)/include/miniupnpc &&\
-	install libminiupnpc.a $($(package)_staging_prefix_dir)/lib
+	install build/libminiupnpc.a $($(package)_staging_prefix_dir)/lib
 endef
